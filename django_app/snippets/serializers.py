38d9fb7d70a3bf6225878f25ebfd96d07220c65e
hanabee337 <hanabee337@gmail.com>
2017-03-14 08:23:11 +0900

    DRF - Totorial 5(관계 & 하이퍼링크 API) : 하이퍼링크로 API 연결하기
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/snippets/serializers.py
+++ django_app/snippets/serializers.py
@@ -12,12 +12,22 @@ from django.contrib.auth.models import User
 # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
 # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 #
-class SnippetSerializer(serializers.ModelSerializer):
+# class SnippetSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source='owner.username')
+
+# class Meta:
+#     model = Snippet
+#     fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
     def create(self, validated_data):
         """
@@ -38,9 +48,16 @@ class SnippetSerializer(serializers.ModelSerializer):
         return instance
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+#
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'snippets')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'username', 'snippets')
