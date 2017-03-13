4c7091138ea82e2b1bf8dc9306bba74e5ebd2e97
hanabee337 <hanabee337@gmail.com>
2017-03-14 08:02:53 +0900

    DRF - Totorial 5(관계 & 하이퍼링크 API) : API의 최상단에 대한 엔드 포인트 만들기
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/snippets/views.py
+++ django_app/snippets/views.py
@@ -4,6 +4,10 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 # class SnippetList(APIView):
 #     """
@@ -106,3 +110,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
