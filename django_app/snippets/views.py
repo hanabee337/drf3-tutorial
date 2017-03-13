2e8e83461fcedb33b265126a89bae08f024d3113
hanabee337 <hanabee337@gmail.com>
2017-03-14 07:49:23 +0900

    DRF - Totorial 4(인증과 권한) : 뷰에 요청 권한 추가하기
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/snippets/views.py
+++ django_app/snippets/views.py
@@ -2,6 +2,7 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.models import Snippet
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
 # class SnippetList(APIView):
@@ -84,6 +85,7 @@ from django.contrib.auth.models import User
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -92,6 +94,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class UserList(generics.ListAPIView):
